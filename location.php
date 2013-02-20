1546625b693d8a51b258529f223c703bfedf41b1
Mikey Allan <mhjallan@gmail.com>
2013-02-20 14:41:28 +0000

    Location - mobile

--- location.php
+++ location.php
@@ -17,66 +17,107 @@
 <!-- LOCATION -->
 <div class="main-content">
 	<article class="section location clearfix">
-				<h1>Location</h1>
-		<h2>Brighton is one of the most vibrant, colourful and creative cities in Europe. It has a rich typographic history, including being the <a href="">birthplace of Eric Gill.</a></h2>
+		<h1>Location</h1>
 		
+		<div id="conf-location">
+			<div class="location-a">
+				<h3>Pre-Party<br/>[ Sponsored by <a href="">MyFonts</a> ]</h3>
+				<address>
+					<span class="conf-location-name">The Basement,</span></br > 
+					24 Kensington Street, Brighton,BN1 4AJ, UK
+				</address>
+			</div>
+		
+			<div class="location-b">
+				<h3 >Conference</h3>
+				<address>
+					<span class="conf-location-name">Brighton Dome Corn Exchange</span> <br />
+					Church Street, Brighton, BN1 1UG, UK
+				</address>
+			</div>
+		
+			<div class="location-c">
+				<h3 >After Party</h3>
+				<address>
+					<span class="conf-location-name">Above Audio</span><br />
+					10 Marine Parade, Brighton BN2 1TL, UK
+				</address>
+			</div>
+		</div>		
+				
+		<div id="city-info">
+			<h2><span>Brighton</span> is one of the most vibrant, colourful and creative cities in Europe. It has a rich typographic history, including being the <a href="http://en.wikipedia.org/wiki/Eric_Gill">birthplace of Eric Gill.</a></h2>
+			<p>Cosmopolitan, compact, energetic, unique, fun, lively, historic, young, exotic and free-spirited, Brighton is a city like no other. 
+			Why not spend the weekend here after the conference! More information is available from <a href="#">visitbrighton.com</a></p>
+		</div>	
+			
 		<div class="map">
-			<iframe width="780" height="270" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?source=embed&amp;hl=en&amp;ie=UTF8&amp;q=corn+exchange,+brighton&amp;fb=1&amp;hq=corn+exchange,&amp;hnear=Brighton,+East+Sussex,+UK&amp;view=map&amp;cid=2326185885725850547&amp;t=m&amp;ll=50.824563,-0.138917&amp;spn=0.009488,0.034332&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
+			<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?source=embed&amp;hl=en&amp;ie=UTF8&amp;q=corn+exchange,+brighton&amp;fb=1&amp;hq=corn+exchange,&amp;hnear=Brighton,+East+Sussex,+UK&amp;view=map&amp;cid=2326185885725850547&amp;t=m&amp;ll=50.824563,-0.138917&amp;spn=0.009488,0.034332&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
 		</div>
+				
+
+		<div id="sleep-food-net">
+			<div id="accommodation">
+				<h4>Accommodation</h4>
 		
-		<div class="location-a">
-			<h3 >Pre-Party</h3>
-			<address>
-			[Sponsored by <a href="">MyFonts</a>]</br >
-			The Basement,</br > 24 Kensington Street,</br > 
-			Brighton,
-			BN1 4AJ, UK
-			</address>
-		</div>
+				<address class="accommodation-a"><span class="conf-location-name">myhotel</span></br>
+				17 Jubilee Street, BN1 1GE</br>
+				+44 (0) 1273 900 300</br>
+				<a href="#">Website</a>
+				</address>
 		
-		<div class="location-b">
-			<h3 >Conference</h3>
-			<address>
-			Brighton Dome Corn <br />
-			Exchange<br />
-			Church Street,<br /> 
-			Brighton, BN1 1UG, UK
-			</address>
-		</div>
+				<address class="accommodation-b" ><span class="conf-location-name">Old Ship Hotel</span></br>
+				31-38 Kings Road, BN1 1GE</br>
+				+44 (0)1273 329 001</br>
+				<a href="#">Website</a>
+				</address>
 		
-		<div class="location-c">
-			<h3 >After Party</h3>
-			<address>
-			Above Audio<br />
-			10 Marine Parade, <br />
-			Brighton<br />
-			BN2 1TL, UK
-			</address>
-		</div>
+				<address class="accommodation-c" ><span class="conf-location-name">Premier Inn</span></br>
+				144 North Street, BN1 1GE</br>
+				+44 (0)87 0990 6340</br>
+				<a href="#">Website</a>
+				</address>
+			</div><!-- END ACCOMMODATION -->
+			<div id="accommodation">
+				<h4>Eating</h4>
 		
-		<p>Cosmopolitan, compact, energetic, unique, fun, lively, historic, young, exotic and free-spirited, Brighton is a city like no other. 
-		Why not spend the weekend here after the conference! More information is available from <a href="#">visitbrighton.com</a></p>
+				<address class="accommodation-a"><span class="conf-location-name">E-Kagen</span></br>
+				22–23 Sydney Street</br>
+				<p>Superb Japanese food served in an unassuming setting</p>
+				</address>
 		
-		<h4><span>Accommodation</span></h4>
+				<address class="accommodation-a"><span class="conf-location-name">E-Kagen</span></br>
+				22–23 Sydney Street</br>
+				<p>Superb Japanese food served in an unassuming setting</p>
+				</address>
 		
-		<address class="accommodation-a"><strong>myhotel</strong></br>
-		17 Jubilee Street, BN1 1GE</br>
-		+44 (0) 1273 900 300</br>
-		<a href="#">Website</a>
-		</address>
+				<address class="accommodation-a"><span class="conf-location-name">E-Kagen</span></br>
+				22–23 Sydney Street</br>
+				<p>Superb Japanese food served in an unassuming setting</p>
+				</address>
 		
-		<address class="accommodation-b" ><strong>Old Ship Hotel</strong></br>
-		31-38 Kings Road, BN1 1GE</br>
-		+44 (0)1273 329 001</br>
-		<a href="#">Website</a>
-		</address>
+				
+			</div><!-- END food -->
+			<div id="accommodation">
+				<h4>Free Wifi</h4>
 		
-		<address class="accommodation-c" ><strong>Premier Inn</strong></br>
-		144 North Street, BN1 1GE</br>
-		+44 (0)87 0990 6340</br>
-		<a href="#">Website</a>
-		</address>
-					
+				<address class="accommodation-a"><span class="conf-location-name">E-Kagen</span></br>
+				22–23 Sydney Street</br>
+				<p>Superb Japanese food served in an unassuming setting</p>
+				</address>
+		
+				<address class="accommodation-a"><span class="conf-location-name">E-Kagen</span></br>
+				22–23 Sydney Street</br>
+				<p>Superb Japanese food served in an unassuming setting</p>
+				</address>
+		
+				<address class="accommodation-a"><span class="conf-location-name">E-Kagen</span></br>
+				22–23 Sydney Street</br>
+				<p>Superb Japanese food served in an unassuming setting</p>
+				</address>
+				
+			</div><!-- END wifi -->				
+		</div>				
 	</article>
 </div>	
 	
