ae7bb47e769efae96c97d160573a426162589872
Mikey Allan <mhjallan@gmail.com>
2013-03-12 13:02:14 +0000

    Ampersand update with speakers

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <header id="top" class="section clearfix" >
-	<a id="jump" href="#nav">Main Navigation</a>
+	<a id="jump" href="#nav">menu</a>
 	
 	<div class="ampersand-logo">
 		<img src="images/ampersand-logo.png" alt="ampersand-logo" />
@@ -7,14 +7,13 @@
 	<h1 class="conf-title">Ampersand 2013</h1>
 	<div class="event-info">
 		<h2 class="description">The Web Typography Conference</h2>
-		<p class="loc-date">Brighton UK,<time datetime="2013-06-28" class="dtstart"> 28th June 2013,</time></p>
 	</div>
 			
 	<div class="tickets clearfix">
-		<a href="#">
-			<p class="cta"><strong class="price">&pound;125</strong><span class="vat">+VAT</span>
-				<span class="registering">Register Now </span> <img src="../images/cta-arrow.png" width="11" height="16" alt="Cta Arrow">
-			</p>
+		<p class="loc-date">Brighton<time datetime="2013-06-28" class="dtstart"> 28 Jun 2013,</time></p>
+		<a href="#" class="cta">
+			<span class="price">&pound;125</span><span class="vat">+VAT</span>
+			<span class="registering">Register Now <img src="../images/cta-arrow.png" alt="Cta Arrow"></span> 
 		</a>
 	</div>
 		
--- index.php
+++ index.php
@@ -19,18 +19,30 @@
 	<!-- SPEAKERS -->
 <div class="main-content">
 	 
-	 <section class="section speakers">
+	 <section class="section sessions">
 		<h1>Speakers & Sessions</h1>
+		<p>Ampersand is an affordable one-day event for 
+knowledgable web designers & type 
+enthusiasts, featuring:</p>
 		<ul class="speakers">
-			<li id="speaker1"><a href="/#">Vincent Connare</a></li>
-			<li id="speaker2"><a href="/#">Jason Santa Maria</a></li>
-			<li id="speaker3"><a href="/#">Jon Tan</a></li>
-			<li id="speaker4"><a href="/#">Jonathan Hoefler</a></li>
-			<li id="speaker5"><a href="/#">David Berlow</a></li>
-			<li id="speaker6"><a href="/#">John Daggett</a></li>
-			<li id="speaker7"><a href="/#">Tim Brown</a></li>
-			<li id="speaker8"><a href="/#">Mark Boulton</a></li>	
-			<li id="speaker9"><a href="/#">Yves Peters</a></li>			
+			<li id="speaker1"><a href="/#"><img src="images/speakers-ben-terrett.jpg" alt="Speakers Ben Terrett"><br/>Ben Terrett</a>	<p class="presentation-title">Presentation Title</p></li>
+			<li id="speaker2"><a href="/#"><img src="images/speakers-mark-boulton.jpg" alt="Speakers Mark Boulton"><br/>Mark Boulton</a>	<p class="presentation-title">Presentation Title</p></li>
+			<li id="speaker3"><a href="/#"><img src="images/speakers-jenn-lukas.jpg" alt="Speakers Jenn Lukas"><br/>Jenn Lukas</a>	<p class="presentation-title">Presentation Title</p></li>
+			<li id="speaker4"><a href="/#"><img src="images/speakers-kutlu-canlioglu.jpg" alt="Speakers Kutlu Canlioglu"><br/>Kutlu Çanlıoğlu</a>	<p class="presentation-title">Presentation Title</p></li>
+			<li id="speaker5"><a href="/#"><img src="images/speakers-nina-stossinger.jpg" alt="Speakers Nina Stossinger"><br/>Nina Stössinger</a>	<p class="presentation-title">Presentation Title</p></li>
+			<li id="speaker6"><a href="/#"><img src="images/speakers-gerry-leonidas.jpg" alt="Speakers Gerry Leonidas"><br/>Gerry Leonidas</a>	<p class="presentation-title">Presentation Title</p></li>
+			<li id="speaker7"><a href="/#"><img src="images/speakers-erik-spiekermann.jpg" alt="Speakers Erik Spiekermann"><br/>Erik Spiekerman</a>	<p class="presentation-title">Presentation Title</p></li>
+			<li id="speaker8"><a href="/#"><img src="images/speakers-richard-rutter.jpg" alt="Speakers Richard Rutter"><br/>Richard Rutter</a>	<p class="presentation-title">Presentation Title</p></li>
+			
+			<!-- <li class="reg-link">
+				<p class="loc-date">Brighton UK,<time datetime="2013-06-28" class="dtstart"> 28th June 2013,</time></p>	
+				<a href="#" class=""><span class="price">&pound;125</span><span class="vat">+VAT</span>
+						<span class="registering cta-speakers">Register Now  <img src="../images/cta-arrow.png" width="11" height="16" alt="Cta Arrow"></span>
+
+				</a>
+				
+			</li> -->
+	
 		</ul>
 		<div class="clearfix">
 	</section>
