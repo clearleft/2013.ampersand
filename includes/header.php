8d56073c9160f500e7725ce8c267148799d34837
Mikey Allan <mhjallan@gmail.com>
2013-03-15 12:06:03 +0000

    weekly review

--- includes/header.php
+++ includes/header.php
@@ -4,17 +4,12 @@
 	<div class="ampersand-logo">
 		<img src="images/ampersand-logo.png" alt="ampersand-logo" />
 	</div>
-	<h1 class="conf-title">Ampersand 2013</h1>
-	<div class="event-info">
-		<h2 class="description">The Web Typography Conference</h2>
-	</div>
+	<h1 class="conf-title">Ampersand 2013<br/><span class="description">The Web Typography Conference</span></h1>
+	
 			
 	<div class="tickets clearfix">
 		<p class="loc-date">Brighton<time datetime="2013-06-28" class="dtstart"> 28 Jun 2013,</time></p>
-		<a href="#" class="cta">
-			<span class="price">&pound;125</span><span class="vat">+VAT</span>
-			<span class="registering">Register Now <img src="images/cta-arrow.png" alt="Cta Arrow"></span> 
-		</a>
+		<a href="#" class="cta">&pound;125<span class="vat"> +VAT</span><span class="registering"> Register Now </span></a>
 	</div>
 		
 	
