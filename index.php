f39d5ccd4aad376611870acf429551ff35a83175
Mikey Allan <mhjallan@gmail.com>
2013-03-15 13:45:29 +0000

    header and home page update

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8" />
-<title>Ampersand 2013 &middot; The Web Typography Conference</title>
-<meta name="description" content="Ampersand is an affordable one-day event for knowledgable web designers and typography enthusiasts, held in Brighton on 15 June 2012." />
-<link rel="shortcut icon" href="/favicon.ico" />
-<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-<link rel="stylesheet" href="css/styles.css">
-
-
-
+	<meta charset="utf-8" />
+	<title>Ampersand 2013 &middot; The Web Typography Conference</title>
+	<meta name="description" content="Ampersand is an affordable one-day event for knowledgable web designers and typography enthusiasts, held in Brighton on 15 June 2012." />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+	<link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body id="wrapper" class="vevent home">
@@ -21,35 +18,21 @@
 	 
 	 <section class="section sessions">
 		<h1>Speakers & Sessions</h1>
-		<p>Ampersand is an affordable one-day event for 
-knowledgable web designers & type 
-enthusiasts, featuring:</p>
+		<p>Ampersand is an affordable one-day event for knowledgable web designers & type enthusiasts, featuring:</p>
 		<ul class="speakers">
-			<li id="speaker1"><a href="/#"><img src="images/speakers-ben-terrett.jpg" alt="Speakers Ben Terrett"><br/>Ben Terrett</a>	<p class="presentation-title">Presentation Title</p></li>
-			<li id="speaker2"><a href="/#"><img src="images/speakers-mark-boulton.jpg" alt="Speakers Mark Boulton"><br/>Mark Boulton</a>	<p class="presentation-title">Presentation Title</p></li>
-			<li id="speaker3"><a href="/#"><img src="images/speakers-jenn-lukas.jpg" alt="Speakers Jenn Lukas"><br/>Jenn Lukas</a>	<p class="presentation-title">Presentation Title</p></li>
-			<li id="speaker4"><a href="/#"><img src="images/speakers-kutlu-canlioglu.jpg" alt="Speakers Kutlu Canlioglu"><br/>Kutlu Çanlıoğlu</a>	<p class="presentation-title">Presentation Title</p></li>
-			<li id="speaker5"><a href="/#"><img src="images/speakers-nina-stossinger.jpg" alt="Speakers Nina Stossinger"><br/>Nina Stössinger</a>	<p class="presentation-title">Presentation Title</p></li>
-			<li id="speaker6"><a href="/#"><img src="images/speakers-gerry-leonidas.jpg" alt="Speakers Gerry Leonidas"><br/>Gerry Leonidas</a>	<p class="presentation-title">Presentation Title</p></li>
-			<li id="speaker7"><a href="/#"><img src="images/speakers-erik-spiekermann.jpg" alt="Speakers Erik Spiekermann"><br/>Erik Spiekerman</a>	<p class="presentation-title">Presentation Title</p></li>
-			<li id="speaker8"><a href="/#"><img src="images/speakers-richard-rutter.jpg" alt="Speakers Richard Rutter"><br/>Richard Rutter</a>	<p class="presentation-title">Presentation Title</p></li>
-			
-			<!-- <li class="reg-link">
-				<p class="loc-date">Brighton UK,<time datetime="2013-06-28" class="dtstart"> 28th June 2013,</time></p>	
-				<a href="#" class=""><span class="price">&pound;125</span><span class="vat">+VAT</span>
-						<span class="registering cta-speakers">Register Now  <img src="../images/cta-arrow.png" width="11" height="16" alt="Cta Arrow"></span>
-
-				</a>
-				
-			</li> -->
-	
+			<li id="speaker1"><a href="/#"><img src="images/speakers-ben-terrett.jpg" alt="Speakers Ben Terrett"><br/>Ben Terrett</a><p class="presentation-title">Presentation Title</p></li>
+			<li id="speaker2"><a href="/#"><img src="images/speakers-mark-boulton.jpg" alt="Speakers Mark Boulton"><br/>Mark Boulton</a><p class="presentation-title">Presentation Title</p></li>
+			<li id="speaker3"><a href="/#"><img src="images/speakers-jenn-lukas.jpg" alt="Speakers Jenn Lukas"><br/>Jenn Lukas</a><p class="presentation-title">Presentation Title</p></li>
+			<li id="speaker4"><a href="/#"><img src="images/speakers-kutlu-canlioglu.jpg" alt="Speakers Kutlu Canlioglu"><br/>Kutlu Çanlıoğlu</a><p class="presentation-title">Presentation Title</p></li>
+			<li id="speaker5"><a href="/#"><img src="images/speakers-erik-spiekermann.jpg" alt="Speakers Erik Spiekermann"><br/>Erik Spiekerman</a><p class="presentation-title">Presentation Title</p></li>
+			<li id="speaker6"><a href="/#"><img src="images/speakers-gerry-leonidas.jpg" alt="Speakers Gerry Leonidas"><br/>Gerry Leonidas</a><p class="presentation-title">Presentation Title</p></li>
+			<li id="speaker7"><a href="/#"><img src="images/speakers-nina-stossinger.jpg" alt="Speakers Nina Stossinger"><br/>Nina Stössinger</a><p class="presentation-title">Presentation Title</p></li>
+			<li id="speaker8"><a href="/#"><img src="images/speakers-richard-rutter.jpg" alt="Speakers Richard Rutter"><br/>Richard Rutter</a><p class="presentation-title">Presentation Title</p></li>
 		</ul>
 		<div class="clearfix">
 	</section>
 	
-</div>
-		
+</div>		
 <?php include("includes/footer.php"); ?>
-
 </body>
 </html>
