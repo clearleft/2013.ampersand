e100d6cc230f7c520c47da2aeb2788393b4745d7
Mikey Allan <mhjallan@gmail.com>
2013-03-18 14:56:19 +0000

    Created Brighton & NYC directory

---
+++ NYC/includes/footer.php
@@ -0,0 +1,103 @@
+<!-- ###############  FOOTER ################### -->
+		
+		<!-- SPONSORS -->
+		<section class="sponsors">
+			<header class="section clearfix">
+				<h1>Our Generous Sponsors</h1>
+			</header>	
+				<div class="premiere">
+					<h2><span>Premiere Sponsors</span></h2>
+					<img src="images/sponsors-fontdeck.png" width="144" height="96" alt="Sponsors Fontdeck">
+					<img src="images/sponsors-typekit.png" width="195" height="96" alt="Sponsors Typekit">
+				
+				</div>
+				<div class="grass-roots">
+					<h2><span>Grass Roots Sponsors</span></h2>
+					<img src="images/sponsors-8faces.png" width="86" height="96" alt="Sponsors 8faces">
+					<img src="images/sponsors-mapalong.png" width="220" height="96" alt="Sponsors Mapalong">
+					<img src="images/sponsors-myfonts.png" width="195" height="96" alt="Sponsors Myfonts">
+					<img src="images/sponsors-webtype.png" width="204" height="96" alt="Sponsors Webtype">
+				</div>
+				<div class="executive">
+					<h2><span>Executive Sponsors</span></h2>
+					<img src="images/sponsors-dtc.png" width="110" height="96" alt="Sponsors Dtc">
+					<img src="images/sponsors-hoefler-frere-jones.png" width="227" height="96" alt="Sponsors Hoefler Frere Jones">
+					<img src="images/sponsors-lls.png" width="207" height="96" alt="Sponsors Lls">
+					<img src="images/sponsors-type-together.png" width="147" height="96" alt="Sponsors Type Together">
+				</div>
+				<div class="media-partners">
+					<h2><span>Media Partners</span></h2>
+					<img src="images/sponsors-eye.png" width="89" height="96" alt="Sponsors Eye">
+					<img src="images/sponsors-wiley.png" width="183" height="96" alt="Sponsors Wiley">
+				
+				</div>
+			</div>
+		</section>
+	
+		<!-- Main NAV -->
+		<nav role="navigation" id="main-nav">
+				
+			<ul id="nav">
+				<li><a class="home-link" href="/" title="Home">Home</a></li>
+				<li><a class="schedule-link" href="/schedule" title="Schedule">Schedule</a></li>
+				<li><a class="workshops-link" href="/workshops" title="location">Workshops</a></li>
+				<li><a class="exhibition-link" href="/exhibition" title="location">Exhibition</a></li>
+				<li><a class="location-link" href="/location" title="location">Location</a></li>
+				<li><a class="sponsors-link" href="/sponsors" title="Sponsors">Sponsors</a></li>
+				<li id="back"><a href="#top">Back to top</a></li>
+			</ul>
+				
+		</nav>
+	
+		<!-- SOCIAL -->
+		<section class="section social">
+			<h3>Keep updated with the latest Ampersand activity</h3>
+			<ul>
+				<li><a  id="twitter" href="http://twitter.com/ampersandconf">Follow <br/><span class="social-subtext">@ampersandconf #ampconf</span></a></li>
+				<li><a id="rss" href="http://feeds.feedburner.com/clearleftevents">Subscribe <br/><span class="social-subtext">to the Clearleft events feed</span></a></li>
+				<li><a id="lanyrd" href="http://lanyrd.com/2011/ampersand/">Lanyrd <br/><span class="social-subtext">Check out Ampersand on Lanyrd</span></a></li>
+				<li><a id="ical" href="webcal://h2vx.com/ics/ampersandconf.com/schedule">+&nbsp;iCal <br/><span class="social-subtext">Add this to your calendar</span></a></li>
+			</ul>
+		</section>   
+
+		<!-- FOOTER NAV -->
+		<div class="section" >
+			<nav class="footer-nav clearfix" >
+				<ul>
+					<li><a class="privacy" href="/privacy" title="Privacy Policy">Privacy</a></li>
+					<li><a class="terms" href="/terms" title="Terms and Conditions">Terms &amp; Conditions</a></li>
+					<li><a class="accessibilty" href="/accessibilty" title="accessibilty">Accessibilty</a></li>
+				</ul>
+			</nav>
+		
+		</div>
+	
+		<!-- CLEARLEFT FOOTER -->
+		<footer >
+			<div class="section">
+				<a href="http://clearleft.com"><img class="clearleft" alt="Clearleft Logo" src="img/clearleft.png" /></a>
+				<p>Presented by <a href="http://clearleft.com" title="Clearleft" >Clearleft</a>, a user experience design consultancy based in Brighton, UK.
+				Email us on <a href="mailto:ampersand@clearleft.com" title="ampersand@clearleft.com">ampersand@clearleft.com</a>
+				</p>
+				<small>Ampersand &copy; 2011 Clearleft Ltd. All rights reserved. Set in flavours of <a href="http://fontdeck.com/typeface/karmina">Karmina</a> and <a href="http://fontdeck.com/typeface/karmina"> Karmina Sans </a>
+				served by <a href="http://fontdeck.com/">Fontdeck.</a></small>
+			
+			</div>
+		</footer>
+
+		<script src="js/plugins.js"></script>
+		<script src="js/script.js"></script>
+	
+		<script type="text/javascript">
+		WebFontConfig = { fontdeck: { id: '29643' } };
+
+		(function() {
+		  var wf = document.createElement('script');
+		  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+		  '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+		  wf.type = 'text/javascript';
+		  wf.async = 'true';
+		  var s = document.getElementsByTagName('script')[0];
+		  s.parentNode.insertBefore(wf, s);
+		})();
+		</script>
---
+++ NYC/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Ampersand 2013 &middot; The Web Typography Conference</title>
+	<meta name="description" content="Ampersand is an affordable one-day event for knowledgable web designers and typography enthusiasts, held in Brighton on 15 June 2012." />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body id="wrapper" class="vevent home">
+
+	<?php include("includes/header.php"); ?>
+
+	<!-- SPEAKERS -->
+<div class="main-content">
+	 
+	 <section class="section sessions">
+		<h1>Speakers & Sessions</h1>
+		<p class="loud">Ampersand is an affordable one-day event for knowledgable web designers & type enthusiasts, featuring:</p>
+		<ul class="speakers">
+			<li class="li-spk1" ><a id="speaker1" href="/#">Ben Terrett</a><p class="presentation-title">Presentation Title</p></li>
+			<li class="li-spk2" ><a id="speaker2" href="/#">Mark Boulton</a><p class="presentation-title">Presentation Title</p></li>
+			<li class="li-spk3" ><a id="speaker3" href="/#">Jenn Lukas</a><p class="presentation-title">Presentation Title</p></li>
+			<li class="li-spk4" ><a id="speaker4" href="/#">Kutlu Çanlıoğlu</a><p class="presentation-title">Presentation Title</p></li>
+			<li class="li-spk5" ><a id="speaker5" href="/#">Erik Spiekerman</a><p class="presentation-title">Presentation Title</p></li>
+			<li class="li-spk6" ><a id="speaker6" href="/#">Gerry Leonidas</a><p class="presentation-title">Presentation Title</p></li>
+			<li class="li-spk7" ><a id="speaker7" href="/#">Nina Stössinger</a><p class="presentation-title">Presentation Title</p></li>
+			<li class="li-spk8" ><a id="speaker8" href="/#">Andy Hume</a><p class="presentation-title">Presentation Title</p></li>
+			<li class="li-spk9" ><a id="speaker9" href="/#">Richard Rutter</a><p class="presentation-title">Presentation Title</p></li>
+		</ul>
+		<div class="clearfix">
+	</section>
+	
+</div>		
+<?php include("includes/footer.php"); ?>
+</body>
+</html>
