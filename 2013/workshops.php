e41e44e7eba5b16daa92b825603bdea61bb0a6fb
Richard Rutter <rich@clearleft.com>
2013-03-23 16:53:47 +0000

    added letterpress workshop and eventbrite links

--- 2013/workshops.php
+++ 2013/workshops.php
@@ -18,17 +18,50 @@
 	 <?php include("includes/nav.php"); ?>
 	 <section class="section workshops">
 		<h1>Workshops</h1>
-		<p class="subheading">Ampersand is now offering a series of day-long workshops. Each workshop costs £445+VAT with attendance strictly limited to twenty five people. Workshop attendees receive complimentary entrance to the Ampersand conference.</p>
+		<p class="subheading">This year we are running workshops in <a href="#wrk1">grid systems</a>, <a href="#wrk2">responsive design</a> and <a href="#wrk3">letterpress</a>. All workshop attendees receive complimentary entrance to the Ampersand conference.</p>
 		<div id="wrk1" class="workshop-item">
 			<h1>Designing Grid Systems with Mark&nbsp;Boulton</h1>
-			<p>The way we make websites has changed. Responsive design has challenged the way we think about layout and their underpinning foundation: grid systems. In this workshop, Mark will take it right back to basics on how to design your grids, what things you need to consider for designing grids for different applications and devices, and how we can practically start working 'content out'.</p>
-<p>Warning: this workshop requires work. In fact, most of the day will be spent working on grids with peer review and critique thrown in for good measure. At the end of the day, you'll know why 12 columns is not always the best grid to use, you'll know the difference between compound grids and modular grids and when to apply them, and – most importantly – you'll have the workflows to start making your own grids from scratch.</p><a href="#" class="cta">Buy Tickets</a>
+			<p>The way we make websites has changed. Responsive design has challenged the way we think about layout and their underpinning foundation: grid systems. In this workshop, Mark will take it right back to basics on how to design your grids, what things you need to consider for designing grids for different applications and devices, and how we can practically start working &#8216;content out&#8217;.</p>
+<p>Warning: this full day workshop requires work. In fact, most of the day will be spent working on grids with peer review and critique thrown in for good measure. At the end of the day, you&#8217;ll know why 12 columns is not always the best grid to use, you&#8217;ll know the difference between compound grids and modular grids and when to apply them, and – most importantly – you&#8217;ll have the workflows to start making your own grids from scratch.</p>
+
+			
+			<h4>Date</h4>
+			<p>Thursday 27th June 2013</p>
+			<h4>Location</h4>
+			<p>Lighthouse, 28 Kensington St, Brighton, BN1 4AJ</p>
+			<h4>Price</h4>
+			<p>only <strong>&pound;499</strong> including <abbr title="Value Added Tax @20%">VAT</abbr></p>
+			<p><a href="http://www.eventbrite.co.uk/event/5977664355" class="cta">Buy Tickets</a></p>
 			
 		</div>
 		<div id="wrk2" class="workshop-item">
 			<h1>Responsive Design with Jeremy&nbsp;Keith</h1>
-			<p>Responsive design is one of the most exciting developments to hit the web for some time. But there’s a common misconception that it involves merely slapping some media queries on to an existing desktop-centric site and labelling the result "mobile-friendly." Nothing could be further from the truth. This workshop will demonstrate that truly effective responsive design must begin with the content first, which is then progressively adapted to a multitude of screen sizes and environments.</p>
-			<p>This workshop will help you to construct web sites that will feel natural on a whole range of devices, from mobile phones to tablets, laptops, e-readers and devices we haven’t even thought of yet.</p><a href="#" class="cta">Buy Tickets</a>
+			<p>Responsive design is one of the most exciting developments to hit the web for some time. But there’s a common misconception that it involves merely slapping some media queries on to an existing desktop-centric site and labelling the result &#8220;mobile-friendly.&#8221; Nothing could be further from the truth. This workshop will demonstrate that truly effective responsive design must begin with the content first, which is then progressively adapted to a multitude of screen sizes and environments.</p>
+			<p>This full day workshop will help you to construct web sites that will feel natural on a whole range of devices, from mobile phones to tablets, laptops, e-readers and devices we haven’t even thought of yet.</p>
+			
+			<h4>Date</h4>
+			<p>Thursday 27th June 2013</p>
+			<h4>Location</h4>
+			<p>Lighthouse, 28 Kensington St, Brighton, BN1 4AJ</p>
+			<h4>Price</h4>
+			<p>only <strong>&pound;499</strong> including <abbr title="Value Added Tax @20%">VAT</abbr></p>
+
+			<p><a href="http://www.eventbrite.co.uk/event/5977513905" class="cta">Buy Tickets</a></p>
+			
+		</div>
+		
+		<div id="wrk3" class="workshop-item">
+			<h1>Letterpress Workshop with Lionel&nbsp;Perks</h1>
+			<p>Take a trip back to an analogue age. The half-day letterpress workshops at Magpie Press are designed to replicate the exact working practice of the letterpress printer. The workshop are project-based &ndash; you&#8217;ll learn how to print a poster and be able take home the results &ndash; and intimate with only two attendees at a time. You&#8217;ll be working with original wooden fonts and lead type under the tuition and supervision of local printing legend, Lionel Perks.</p>
+
+			
+			<h4>Dates</h4>
+			<p>Wednesday 26th &ndash; Thursday 27th June 2013</p>
+			<h4>Location</h4>
+			<p>Magpie Press, 94a Gloucester Road, Brighton, BN1 4AP</p>
+			<h4>Price</h4>
+			<p>only <strong>&pound;249</strong> including <abbr title="Value Added Tax @20%">VAT</abbr></p>
+			<p><a href="http://www.eventbrite.co.uk/event/5977680403" class="cta">Buy Tickets</a></p>
 			
 		</div>
 		<div class="clearfix">
