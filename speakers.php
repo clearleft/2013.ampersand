47b5af6b18f5ad8ebd6d9be685c898cfae0273f2
Richard Rutter <rich@clagAir-2.local>
2013-03-25 23:06:30 +0000

    added smartquotes

--- speakers.php
+++ speakers.php
@@ -25,12 +25,12 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
 
 <script type="text/javascript">
-	$(document).ready(function () {  
+	$(document).ready(function () {
 	  var top = $('.speaker-nav').offset().top - parseFloat($('.speaker-nav').css('marginTop').replace(/auto/, 0));
 	  $(window).scroll(function (event) {
 	    // what the y position of the scroll is
 	    var y = $(this).scrollTop();
-  
+
 	    // whether that's below the form
 	    if (y >= top) {
 	      // if so, ad the fixed class
@@ -40,7 +40,7 @@
 	      $('.speaker-nav').removeClass('fixed');
 	    }
 	  });
-	  
+
 	  $('a[href^="#"]').bind('click.smoothscroll',function (e) {
 	      e.preventDefault();
 	      var target = this.hash;
@@ -52,8 +52,8 @@
 	      });
 	  });
 	});
-	
-	
+
+
 </script>
 
 </head>
@@ -64,19 +64,19 @@
 
 
 <div class="main-content">
-	
+
 	<?php include("includes/nav.php"); ?>
-	
+
     <section class="speakers-container" id="erik-spiekermann">
-	
+
    	<div class="speaker-copy" >
     		<h1>Keynote</h1>
     		<p></p>
-	
+
    		<h2>Erik Spiekermann</h2>
     		<p class="speaker-bio">Erik Spiekermann is information architect, type designer and author of books and articles on type and typography. Two of his typefaces, FF Meta and ITC Officina, are considered to be modern classics.  In 1979 he founded MetaDesign and in 1989 FontShop. Today he is managing partner and creative director of Edenspiekermann. He is also Honorary Royal Designer for Industry in Britain.</p>
     	</div>	<!-- END SPEAKER-BIO -->
-	
+
    	<div class="speaker-more">
     		<h3>More Info</h3>
     		<ul>
@@ -85,17 +85,17 @@
     		</ul>
     	</div><!-- END MORE-BIO -->
    </section><!-- ########  END SPIEKERMANN ###### -->
-   
+
    <section class="speakers-container" id="mark-boulton">
-	
+
   		<div class="speaker-copy" >
    		<h1>Typography in Responsive Design</h1>
    		<p></p>
-	
+
   		<h2>Mark Boulton</h2>
    		<p class="speaker-bio">Mark Boulton is a web designer and author, and member of the ISTD. He is the founder of Mark Boulton Design, and co-founder of indie publisher Five Simple Steps. Sometimes outspoken, always passionate, Mark is obsessed with applying the fundamentals of good typographic design on the web. Mark lives in South Wales with his wife and daughter, a small collection of neglected bonsai and a guitar he wish he owned when he was 18.</p>
    	</div>	<!-- END SPEAKER-BIO -->
-	
+
   		<div class="speaker-more">
   	 		<h3>More Info</h3>
   	 		<ul>
@@ -104,17 +104,17 @@
   	 		</ul>
   	 	</div><!-- END MORE-BIO -->
   </section><!-- ########  END SPEAKER ###### -->
-  
+
   <section class="speakers-container" id="gerry-leonidas">
-	
+
  	<div class="speaker-copy" >
   		<h1>The Newest New Typography</h1>
   		<p></p>
-	
+
  		<h2>Gerry Leonidas</h2>
   		<p class="speaker-bio">Gerry is a consultant and Senior Lecturer of typography and typeface design at the University of Reading. His consultancy focuses on typeface design and typography with particular expertise in Greek typography. Gerry’s perspective is one of placing typography in a wider context, helping designers develop an understanding of the basic principles, and gain an insight into the potential for originality. </p>
   	</div>	<!-- END SPEAKER-BIO -->
-	
+
  	<div class="speaker-more">
   		<h3>More Info</h3>
   		<ul>
@@ -124,18 +124,18 @@
   		</ul>
   	</div><!-- END MORE-BIO -->
  </section><!-- ########  END SPEAKER ###### -->
-  
-	 
+
+
 	 <section class="speakers-container"  id="ben-terrett">
-		
+
  		<div class="speaker-copy" >
 	 		<h1>Designing GOV.UK</h1>
 	 		<p></p>
-		
+
 			<h2>Ben Terrett</h2>
-	 		<p class="speaker-bio">Ben is Head of Design at the Government Digital Service, in the Cabinet Office for the UK Government. Before that he was Design Director at Wieden & Kennedy London. In 2010 Ben won the Graphics category in the Design Museum's Designs Of The Year Awards. Ben has spoken about design on Radio 4's Today programme and has been a D&amp;AD judge several times.</p>
+	 		<p class="speaker-bio">Ben is Head of Design at the Government Digital Service, in the Cabinet Office for the UK Government. Before that he was Design Director at Wieden & Kennedy London. In 2010 Ben won the Graphics category in the Design Museum&#8217;s Designs Of The Year Awards. Ben has spoken about design on Radio 4&#8217;s Today programme and has been a D&amp;AD judge several times.</p>
 	 	</div>	<!-- END SPEAKER-BIO -->
-		
+
  		<div class="speaker-more">
  	 		<h3>More Info</h3>
  	 		<ul>
@@ -145,17 +145,17 @@
  	 		</ul>
  	 	</div><!-- END MORE-BIO -->
 	</section><!-- ########  END SPEAKER ###### -->
-	
+
     <section class="speakers-container" id="kutlu-canlioglu">
-	
+
    	<div class="speaker-copy" >
     		<h1>Designing BBC Worldwide</h1>
     		<p></p>
-	
+
    		<h2>Kutlu Çanlıoğlu</h2>
-    		<p class="speaker-bio">Kutlu is Creative Director of BBC iPlayer where he leads the user experience and design team. Before leading the iPlayer team, Kutlu was the Creative Director for the BBC's Global Experience Language and responsible for championing the role of craftsmanship around typography. </p>
+    		<p class="speaker-bio">Kutlu is Creative Director of BBC iPlayer where he leads the user experience and design team. Before leading the iPlayer team, Kutlu was the Creative Director for the BBC&#8217;s Global Experience Language and responsible for championing the role of craftsmanship around typography. </p>
     	</div>	<!-- END SPEAKER-BIO -->
-	
+
    	<div class="speaker-more">
     		<h3>More Info</h3>
     		<ul>
@@ -166,15 +166,15 @@
 
 
     <section class="speakers-container" id="nina-stoessinger">
-	
+
    	<div class="speaker-copy" >
     		<h1>Being Ernestine</h1>
     		<p></p>
-	
+
    		<h2>Nina Stössinger</h2>
     		<p class="speaker-bio">Nina is a self-employed designer based in Basel, where she works in graphic and web design, typography and book design. Nina also teaches screen typography at FH Dresden and is a contributor to We Love Typography. Her nights and weekends are often spent working on her own type projects, resulting in the release of her first commercial typeface in 2011.</p>
     	</div>	<!-- END SPEAKER-BIO -->
-	
+
    	<div class="speaker-more">
     		<h3>More Info</h3>
     		<ul>
@@ -183,19 +183,19 @@
     		</ul>
     	</div><!-- END MORE-BIO -->
    </section><!-- ########  END SPEAKER ###### -->
-   
-   
-   
+
+
+
     <section class="speakers-container" id="andy-hume">
-   	
+
    	<div class="speaker-copy" >
     		<h1>Web Fonts as a Progressive Enhancement</h1>
     		<p></p>
-   	
+
    		<h2>Andy Hume</h2>
     		<p class="speaker-bio">Andy builds web sites and web applications. He currently works for the Guardian in London as client-side architect in the Digital Development team. Over the years Andy has written code and consulted for clients including the BBC, Channel 4, NBC, Fontdeck, Mozilla, and eBay. Back in the day, he regularly wrote for the now-defunct UsableType blog.</p>
     	</div>	<!-- END SPEAKER-BIO -->
-   	
+
    	<div class="speaker-more">
     		<h3>More Info</h3>
     		<ul>
@@ -204,18 +204,18 @@
     		</ul>
     	</div><!-- END MORE-BIO -->
    </section><!-- ########  END SPEAKER ###### -->
- 
+
 
  <section class="speakers-container" id="jenn-lukas">
-	
+
 	<div class="speaker-copy" >
  		<h1>On Icon Fonts and Working with Designers</h1>
  		<p></p>
-	
+
 		<h2>Jenn Lukas</h2>
  		<p class="speaker-bio">Jenn Lukas is a web developer working with Happy Cog in Philadelphia. She writes for The Nerdary and has a regular column in .net magazine. When she’s not crafting sites with the finest of web standards, Jenn teaches HTML and CSS for GirlDevelopIt.</p>
  	</div>	<!-- END SPEAKER-BIO -->
-	
+
 	<div class="speaker-more">
  		<h3>More Info</h3>
  		<ul>
@@ -224,7 +224,7 @@
  		</ul>
  	</div><!-- END MORE-BIO -->
 </section><!-- ########  END SPEAKER ###### -->
-	
+
 	<div class="speaker-nav">
 		<h2>Speakers &amp; Sessions</h2>
 		<ul>
@@ -238,9 +238,9 @@
 			<li><a href="#jenn-lukas">Jenn Lukas</a></li>
 		</ul>
 	</div><!-- END SPEAKER-NAV -->
-	
+
 </div>
-		
+
 <?php include("includes/footer.php"); ?>
 
 </body>
