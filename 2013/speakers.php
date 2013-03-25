a7bcabbd10b2e3fbbbf7dd09de42a43d4c46f0c2
Mikey Allan <mhjallan@gmail.com>
2013-03-25 10:00:56 +0000

    Speakers smooth scrolling

--- 2013/speakers.php
+++ 2013/speakers.php
@@ -27,7 +27,20 @@
 	      $('.speaker-nav').removeClass('fixed');
 	    }
 	  });
+	  
+	  $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+	      e.preventDefault();
+	      var target = this.hash;
+	          $target = $(target);
+	      $('html, body').stop().animate({
+	          'scrollTop': $target.offset().top
+	      }, 500, 'swing', function () {
+	          window.location.hash = target;
+	      });
+	  });
 	});
+	
+	
 </script>
 
 </head>
